ff660da3299f380178ba3038b90080a49bbbeb6e
v2538n <v2538n@gmail.com>
2020-05-02 08:35:01 +0300

    little change
    Some little change

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
 var gulp         = require('gulp'),
-		sass         = require('gulp-sass'),
-		browserSync  = require('browser-sync').create(),
-		concat       = require('gulp-concat'),
-		uglify       = require('gulp-uglify-es').default,
-		cleancss     = require('gulp-clean-css'),
-		autoprefixer = require('gulp-autoprefixer'),
-		rsync        = require('gulp-rsync'),
-		newer        = require('gulp-newer'),
-		rename       = require('gulp-rename'),
-		responsive   = require('gulp-responsive'),
-		del          = require('del');
+	sass         = require('gulp-sass'),
+	browserSync  = require('browser-sync').create(),
+	concat       = require('gulp-concat'),
+	uglify       = require('gulp-uglify-es').default,
+	cleancss     = require('gulp-clean-css'),
+	autoprefixer = require('gulp-autoprefixer'),
+	rsync        = require('gulp-rsync'),
+	newer        = require('gulp-newer'),
+	rename       = require('gulp-rename'),
+	responsive   = require('gulp-responsive'),
+	del          = require('del');
 
 // Local Server
 gulp.task('browser-sync', function() {
@@ -18,8 +18,8 @@ gulp.task('browser-sync', function() {
 			baseDir: 'app'
 		},
 		notify: false,
-		// online: false, // Work offline without internet connection
-		// tunnel: true, tunnel: 'projectname', // Demonstration page: http://projectname.localtunnel.me
+		// online: true, // Work offline without internet connection
+		tunnel: true, tunnel: 'projectname', // Demonstration page: http://projectname.localtunnel.me
 	})
 });
 function bsReload(done) { browserSync.reload(); done(); };
